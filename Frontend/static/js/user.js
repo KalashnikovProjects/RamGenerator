bd40672b742e8e8dd1f1f962a9109d4c4d114238
Maks <kalasmaks97@gmail.com>
2024-08-28 02:20:56 +0400

    Frontend: user.js ram generation working

--- Frontend/static/js/user.js
+++ Frontend/static/js/user.js
@@ -4,7 +4,7 @@ function show404() {
     document.getElementById("user-section").innerHTML = `<div class="text-center" style="margin-top: 32vh">
     <h1 class="m-1 mb-2">Такого пользователя не существует</h1>
     <a class="tap-text" style="font-size: 1.5rem" href="/">Вернуться на главную</a>
-    </div>`
+    </div>`;
 }
 
 async function loadUserInfo(username) {
@@ -16,13 +16,13 @@ async function loadUserInfo(username) {
         if (response.ok) {
             userInfo = await response.json();
             userInfo.avatar_url = await loadAvatar(userInfo.username, userInfo.avatar_ram_id);
-            userInfo.own = (!!user && user.username === userInfo.username)
+            userInfo.own = (!!user && user.username === userInfo.username);
             return userInfo
         } else {
-            show404()
+            show404();
         }
     } catch (error) {
-        show404()
+        show404();
     }
 }
 
@@ -37,54 +37,54 @@ async function loadUserRams(username) {
             userRams = await response.json();
             return userRams
         } else {
-            show404()
+            show404();
         }
     } catch (error) {
-        show404()
+        show404();
     }
 }
 
 
 let loadingUserInfo = true
 let loadingUserRams = true
-const userInfoUsername = window.location.pathname.split("/")[2]
+const userInfoUsername = window.location.pathname.split("/")[2];
 
-loadUserInfo(userInfoUsername).then((user) => { if (user!==undefined) { loadingUserInfo = false }})
-loadUserRams(userInfoUsername).then((rams) => { if (rams!==undefined) { loadingUserRams = false }})
+loadUserInfo(userInfoUsername).then((user) => { if (user!==undefined) { loadingUserInfo = false }});
+loadUserRams(userInfoUsername).then((rams) => { if (rams!==undefined) { loadingUserRams = false }});
 
 
 async function displayUserInfo() {
     if (loadingUserInfo) {
-        setTimeout(displayUserInfo, 5)
+        setTimeout(displayUserInfo, 5);
         return;
     }
     const [[x1, y1], [x2, y2]] = userInfo.avatar_box;
-    const size = Math.abs(y1 - y2)
+    const size = Math.abs(y1 - y2);
     let imageStyle =  `
     background-size: ${100 / size}%;
     background-position: ${(Math.min(y1, y2) + size / 2) * 100}% ${(Math.min(x1, x2) + size / 2) * 100}%;
-    background-image: url(${userInfo.avatar_url});`
-    let imageOnclick = ""
+    background-image: url(${userInfo.avatar_url});`;
+    let imageOnclick = "";
     if (userInfo.avatar_ram_id !== 0) {
-        imageStyle += "cursor: pointer;"
-        imageOnclick = `onclick="location.hash='#ram-${userInfo.avatar_ram_id}'"`
+        imageStyle += "cursor: pointer;";
+        imageOnclick = `onclick="location.hash='#ram-${userInfo.avatar_ram_id}'"`;
     }
     let res = `
     <div class="user-card-avatar" ${imageOnclick} style="${imageStyle}"></div>
-    <h3 class="user-card-username">${userInfo.username}</h3>`
+    <h3 class="user-card-username">${userInfo.username}</h3>`;
     if (userInfo.own) {
         res += `<button class="button-user left-button-user row first-button-user" onclick="location.hash='#generate-ram'; ramGenerator = new Generator()">Сгенерировать барана</button>
                 <button class="button-user left-button-user row " disabled onclick="location.href='/trade'">Обменять баранов</button>
-                <button class="button-user left-button-user row last-button-user" onclick="location.hash='#settings'">Настройки аккаунта</button>`
+                <button class="button-user left-button-user row last-button-user" onclick="location.hash='#settings'">Настройки аккаунта</button>`;
     } else {
 
     }
-    document.getElementById("user-card").innerHTML = res
+    document.getElementById("user-card").innerHTML = res;
 }
 
 async function displayUserRams() {
     if (loadingUserRams) {
-        setTimeout(displayUserRams, 5)
+        setTimeout(displayUserRams, 5);
         return;
     }
     if (userRams.length === 0) {
@@ -93,75 +93,115 @@ async function displayUserRams() {
             <div class="text-center top-to-center">
                 <h2 class="m-1 mb-2">У вас пока-что нету баранов</h2>
                 <a class="button-user" onclick="location.hash='#generate-ram';ramGenerator = new Generator()">Сгенерировать барана</a>
-            </div>`
+            </div>`;
         } else {
             document.getElementById("rams").innerHTML = `
             <div class="text-center top-to-center">
                 <h2 class="m-1 mb-2">У этого пользователя нету баранов</h2>
-            </div>`
+            </div>`;
         }
         return
     }
-    document.getElementById("rams").innerHTML = `<div class="rams-list row row-cols-auto g-3 d-flex justify-content-center"></div>`
+    document.getElementById("rams").innerHTML = `<div class="rams-list row row-cols-auto g-3 d-flex justify-content-center"></div>`;
     for (let ram in userRams) {
-        // TODO
+        // TODO рендер баранов
     }
 }
 
 class Clicker {
-    constructor(clickElemId, sendClicksCallback) {
-        this.clickElemId = clickElemId
-        this.sendClicksCallback = sendClicksCallback
-
-        this.clicksCount = 0
-        this.lastSendCount = 0
+    constructor(clickElemId, onclickCallback, sendClicksCallback) {
+        this.clickElemId = clickElemId;
+        this.onclickCallback = onclickCallback;
+        this.sendClicksCallback = sendClicksCallback;
+        this.clicksCount = 0;
+        this.lastSendCount = 0;
         this.lastSendTime = new Date();
+        this.onclickBinded = this.onclick.bind(this);
 
-        document.getElementById(clickElemId).addEventListener("pointerup", this.onclick.bind(this))
+        document.getElementById(clickElemId).addEventListener("pointerup", this.onclickBinded);
+        this.sendClicksInterval = setInterval(this.sendClicks.bind(this), 15000);
+    }
+
+    sendClicks(forced=false) {
+        let now = new Date();
+
+        if (!forced && (this.lastSendCount >= this.clicksCount || (+now - +this.lastSendTime) < 5 * 100)) {
+            return
+        }
+        this.clicksIferror = this.clicksCount;
+        this.sendClicksCallback(this.clicksCount-this.lastSendCount);
+        this.lastSendCount = this.clicksCount;
+        this.lastSendTime = now;
+    }
+
+    rollbackErrorClicks() {
+        this.clicksCount = this.clicksIferror;
     }
 
     onclick() {
-        this.clicksCount++
+        this.clicksCount++;
+        if (this.clicksCount - this.lastSendCount >= 50) {
+            this.sendClicks();
+        }
+        this.onclickCallback(this.clicksCount);
     }
 
     close() {
+        this.sendClicks(true);
+        clearInterval(this.sendClicksInterval);
+        document.getElementById(this.clickElemId).removeEventListener("pointerup", this.onclickBinded);
+    }
+}
 
+class TargetedClicker extends Clicker {
+    constructor(target, endCallback, clickElemId, onclickCallback, sendClicksCallback) {
+        super(clickElemId, onclickCallback, sendClicksCallback);
+        this.target = target;
+        this.endCallback = endCallback;
+    }
+
+    onclick() {
+        super.onclick();
+        if (this.clicksCount >= this.target) {
+            this.close();
+            this.endCallback()
+        }
     }
 }
 
 class Generator {
     constructor() {
-        this.initialize()
+        this.initialize();
     }
 
     async initialize() {
         while (loadingUserInfo) {
-            await sleep(5)
+            await sleep(5);
         }
         if (!userInfo.own) {
-            location.hash = ""
+            location.hash = "";
         }
 
         document.querySelector("#generate-ram .popup-menu").innerHTML = `
              <h4 id="generation-title" class="text-center">Генерация барана</h2>
              <div id="generation-content"></div>`
-        this.connectWs()
+        this.connectWs();
     }
 
 
     connectWs() {
-        let apiUrl = new URL(API_URL)
-        apiUrl.protocol = "ws"
+        let apiUrl = new URL(API_URL);
+        apiUrl.protocol = "ws";
         this.websocket = new WebSocket(`${apiUrl}/users/${user.username}/ws/generate-ram`);
-        this.websocket.onopen = this._onopen.bind(this)
-        this.websocket.onmessage = this._onmessage.bind(this)
-        this.websocket.onclose = this._onclose.bind(this)
-        this.websocket.onerror = this._onerror.bind(this)
+        this.websocket.onopen = this._onopen.bind(this);
+        this.websocket.onmessage = this._onmessage.bind(this);
+        this.websocket.onclose = this._onclose.bind(this);
+        this.websocket.onerror = this._onerror.bind(this);
     }
 
     sendPrompt() {
-        const promptEl = document.getElementById("prompt")
-        const prompt = promptEl.value
+        const promptEl = document.getElementById("prompt");
+        const prompt = promptEl.value;
         if (!prompt.trim().length) {
             // TODO: error
             return
@@ -173,37 +213,69 @@ class Generator {
         let error;
         switch (data.code) {
             case 401:
-                error = "Проблема с авторизацией, попробуйте выйти и зайти в аккаунт"
+                error = "Проблема с авторизацией, попробуйте выйти и зайти в аккаунт";
                 break;
             case 403:
-                error = "Вы не можете генерировать баранов для других пользователей"
+                error = "Вы не можете генерировать баранов для других пользователей";
                 break
             case 409:
-                error = "Похоже, вы тапаете барана в другой вкладке, или на другом устройстве. Попробуйте повторить позже."
+                error = "Похоже, вы тапаете барана в другой вкладке, или на другом устройстве. Попробуйте повторить позже.";
                 break
-            //TODO
+            case 400:
+                switch (data.error) {
+                    case "invalid clicks":
+                        this.targetedClicker.rollbackErrorClicks();
+                        // TODO сообщение об ошибке
+                        break;
+                    default:
+                        console.log("Unknown error", data.code, data.error);
+                        error = `Unknown error ${data.code} ${data.error}`;
+                        break;
+                }
+                break;
+            //TODO либо закрывать popup, либо выводить сообщение об ошибке (с кнопкой ещё раз и без)
             default:
-                console.log("Unknown error", data.error);
-                error = `Unknown error ${data.error}`
+                console.log("Unknown error", data.code, data.error);
+                error = `Unknown error ${data.code} ${data.error}`;
                 break;
         }
         if (error) {
-            const content = document.getElementById("generation-content")
+            const content = document.getElementById("generation-content");
             content.innerHTML = `
                 <div class="text-center popup-error" style="position: relative;top: 30%">
                     <h5>${error}</h5>
                     <a class="tap-text" style="font-size: 1rem" onclick="closePopup()">ОК</a>
                 </div>
-                `
+                `;
         }
     }
 
     close() {
-        document.getElementById("generation-content").innerHTML = ``
-        this.websocket.close()
-        needClicks = undefined
-        clicks = 0
-        ramGenerator = undefined
+        if (this.websocket.readyState !== WebSocket.CLOSED) {
+            if (this.targetedClicker) {
+                this.targetedClicker.close();
+            }
+            this.websocket.close();
+        }
+
+        document.getElementById("generation-content").innerHTML = ``;
+        needClicks = undefined;
+        clicks = 0;
+        ramGenerator = undefined;
+    }
+
+    endClicker () {
+        this.targetedClicker = undefined;
+    }
+
+    changeClicksValue(value) {
+        const clicksEl = document.getElementById("clicks");
+        clicksEl.innerText = `${value}/${this.needClicks}`;
+    }
+
+    sendClicks(value) {
+        console.log(value)
+        this.websocket.send(`${value}`);
     }
 
     _onopen(event) {
@@ -211,10 +283,16 @@ class Generator {
     }
 
     _onmessage(event) {
-        let data = JSON.parse(event.data)
-        console.log("Message: ", data)
+        let data = JSON.parse(event.data);
+        console.log("Message: ", data);
         if (data.error) {
-            this.handleWSError(data)
+            this.handleWSError(data);
+            return
+        }
+        if (data.id) {
+            this.close();
+            // TODO анимация завершения
+            openRam(data.id);
             return
         }
         const content = document.getElementById("generation-content");
@@ -223,35 +301,40 @@ class Generator {
                 content.innerHTML = `
                 <label class="mb-4 prompt-label">
                     Введите запрос для вашего первого барана<br>
-                    <input class="prompt-input" id="prompt" type="text" name="" placeholder="" maxlength="30">
+                    <input class="prompt-input" id="prompt" type="text" maxlength="30">
                 </label>
                 <div id="generation-bottom">
                     <button id="enter-prompt" class="button-user" onclick="ramGenerator.sendPrompt()">Далее</button>
-                </div>`
+                </div>`;
 
                 break;
             case "need ram prompt":
                 content.innerHTML = `
                 <label class="mb-4 prompt-label">
                     Введите запрос для барана<br>
-                    <input class="prompt-input" id="prompt" type="text" name="" placeholder="" maxlength="30">
+                    <input class="prompt-input" id="prompt" type="text" maxlength="30">
                 </label>
                 <div id="generation-bottom">
                     <button id="enter-prompt" class="button-user" onclick="ramGenerator.sendPrompt()">Далее</button>
-                </div>`
+                </div>`;
                 break;
             case "need clicks":
-                needClicks = data.clicks
-                clicks = 0
+                this.needClicks = data.clicks;
                 content.innerHTML = `
-                    <h4 class="text-center tap-label">Тапните ${needClicks} раз, чтобы сгенерировать барана</h4>
-                    <img src="/static/img/rambox1.png" class="img-fluid tap-generate-img" alt="" style="cursor: pointer;">
-                    <h1 id="clicks" class="text-center">${clicks}/${needClicks}</h1>`
-                //TODO
+                    <h4 class="text-center tap-label">Тапните ${this.needClicks} раз, чтобы сгенерировать барана</h4>
+                    <img id="clicker" src="/static/img/rambox1.png" class="img-fluid tap-generate-img" alt="" style="cursor: pointer;">
+                    <h1 id="clicks" class="text-center">0/${this.needClicks}</h1>`;
+                this.targetedClicker = new TargetedClicker(
+                    this.needClicks,
+                    this.endClicker.bind(this),
+                    "clicker",
+                    this.changeClicksValue.bind(this),
+                    this.sendClicks.bind(this));
                 break;
-            //TODO
+            //TODO "image generated", "success clicked"
         }
     }
+
 
     _onclose(event) {
         if (event.wasClean) {
@@ -259,6 +342,7 @@ class Generator {
         } else {
             let reason;
             switch (event.code) {
+                // TODO
                 case 1000:
                     reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
                     break;
@@ -305,33 +389,52 @@ class Generator {
     }
 
     _onerror(error) {
-        const content = document.getElementById("generation-content")
+        const content = document.getElementById("generation-content");
         content.innerHTML = `
                 <div class="text-center popup-error" style="margin-top: 40%">
                     <h4>Ошибка подключения, возможно вы открываете эту страницу слишком часто.</h4>
                     <a class="tap-text" style="font-size: 1rem" onclick="closePopup()">ОК</a>
-                </div>`
+                </div>`;
     };
 }
 
 function bindUserPopups() {
     for (const elem of document.getElementsByClassName("user-popup")) {
         elem.addEventListener("mousedown", function (event) {
-            if(event.target.classList.contains("user-popup")) {closePopup()}})
+            if(event.target.classList.contains("user-popup")) {
+                closePopup()
+            }}
+        );
     }
 }
 
 function checkHash() {
     if (location.hash === "#generate-ram" && user.username === userInfoUsername) {
-        ramGenerator = new Generator()
+        ramGenerator = new Generator();
+    }
+    if (location.hash === "#ram") {
+
     }
 }
 
 
 function closePopup() {
     if (ramGenerator) {
-        ramGenerator.close()
+        ramGenerator.close();
     }
-    location.hash = ""
+    location.hash = "";
 }
 window.addEventListener('beforeunload', closePopup);
+
+function ram(id) {
+
+}
+
+async function openRam(id) {
+    location.hash = "#ram";
+
+    const url = new URL(location);
+    url.searchParams.set("ram-id", `${id}`);
+    history.pushState({}, "", url);
+    ram(id)
+}
