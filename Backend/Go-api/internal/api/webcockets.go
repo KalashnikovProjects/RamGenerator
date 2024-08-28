3441a2937b9a0f1fca99654747b755b3f3805ad3
KalashnikovProjects <kalasmaks97@yandex.com>
2024-08-29 01:48:59 +0400

    websockets.go Add mutex for websocket send

--- Backend/Go-api/internal/api/webcockets.go
+++ Backend/Go-api/internal/api/webcockets.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -39,6 +40,26 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+func WebsocketSend(ctx context.Context, ws *websocket.Conn, message string) error {
+	mutex, ok := ctx.Value("websocketSendMutex").(*sync.Mutex)
+	if !ok {
+		return ws.WriteMessage(websocket.TextMessage, []byte(message))
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	return ws.WriteMessage(websocket.TextMessage, []byte(message))
+}
+
+func WebsocketSendJSON(ctx context.Context, ws *websocket.Conn, value interface{}) error {
+	mutex, ok := ctx.Value("websocketSendMutex").(*sync.Mutex)
+	if !ok {
+		return ws.WriteJSON(value)
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+	return ws.WriteJSON(value)
 }
 
 func ValidateClickData(clicks int, lastClicks time.Time) bool {
@@ -82,23 +103,23 @@ func PingOrCancelContext(ctx context.Context, ws *websocket.Conn, cancel func())
 	}
 }
 
-func wsFirstMessageAuthorization(ws *websocket.Conn) (int, error) {
+func wsFirstMessageAuthorization(ctx context.Context, ws *websocket.Conn) (int, error) {
 	ws.SetReadDeadline(time.Now().Add(time.Second * 2))
 
 	messageType, wsMessage, err := ws.ReadMessage()
 	if err != nil {
-		ws.WriteJSON(wsError{"read message error", 400})
+		WebsocketSendJSON(ctx, ws, wsError{"read message error", 400})
 		return 0, err
 	}
 	if messageType != websocket.TextMessage {
-		ws.WriteJSON(wsError{"invalid message type", 400})
+		WebsocketSendJSON(ctx, ws, wsError{"invalid message type", 400})
 		return 0, err
 	}
 
 	token := string(wsMessage)
 	userId, err := auth.LoadUserIdFromToken(token)
 	if err != nil {
-		ws.WriteJSON(wsError{"unauthorized, first message must be token", 401})
+		WebsocketSendJSON(ctx, ws, wsError{"unauthorized, first message must be token", 401})
 		return 0, err
 	}
 	ws.SetReadDeadline(time.Time{})
@@ -110,14 +131,14 @@ func checkWsUserAccess(ctx context.Context, db database.SQLTXQueryExec, ws *webs
 	user, err := database.GetUserContext(ctx, db, userId)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			ws.WriteJSON(wsError{"can't recognize your permissions, please relogin", 401})
+			WebsocketSendJSON(ctx, ws, wsError{"can't recognize your permissions, please relogin", 401})
 			return entities.User{}, err
 		}
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return entities.User{}, err
 	}
 	if user.Username != params["username"] {
-		ws.WriteJSON(wsError{"you can't tap another user ram", 403})
+		WebsocketSendJSON(ctx, ws, wsError{"you can't tap another user ram", 403})
 		return entities.User{}, err
 	}
 	return user, nil
@@ -126,34 +147,34 @@ func checkWsUserAccess(ctx context.Context, db database.SQLTXQueryExec, ws *webs
 func checkWsRamAccess(ctx context.Context, db database.SQLTXQueryExec, ws *websocket.Conn, user entities.User, params map[string]string) (entities.Ram, error) {
 	ramId, err := strconv.Atoi(params["id"])
 	if err != nil {
-		ws.WriteJSON(wsError{"ram id must be integer", 400})
+		WebsocketSendJSON(ctx, ws, wsError{"ram id must be integer", 400})
 		return entities.Ram{}, err
 	}
 
 	dbRam, err := database.GetRamContext(ctx, db, ramId)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			ws.WriteJSON(wsError{"ram not found", 404})
+			WebsocketSendJSON(ctx, ws, wsError{"ram not found", 404})
 			return entities.Ram{}, err
 		}
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return entities.Ram{}, err
 	}
 	if user.Id != dbRam.UserId {
-		ws.WriteJSON(wsError{"it's not your ram", 403})
+		WebsocketSendJSON(ctx, ws, wsError{"it's not your ram", 403})
 		return entities.Ram{}, err
 	}
 	return dbRam, nil
 }
 
-func checkWsCanGenerateRam(ws *websocket.Conn, user entities.User) error {
+func checkWsCanGenerateRam(ctx context.Context, ws *websocket.Conn, user entities.User) error {
 	ramsGeneratedLastDay := user.CalculateRamsGeneratedLastDay(config.Conf.Generation.TimeBetweenDaily)
 	if ramsGeneratedLastDay == 0 {
 		return nil
 	}
 	if ramsGeneratedLastDay >= len(config.Conf.Clicks.DailyRams) {
 		targetTime := user.DailyRamGenerationTime + config.Conf.Generation.TimeBetweenDaily*60
-		ws.WriteJSON(wsErrorRateLimit{fmt.Sprintf("you can generate only %d rams per day, you can generate next in %d (unix)", len(config.Conf.Clicks.DailyRams), targetTime), 429, targetTime})
+		WebsocketSendJSON(ctx, ws, wsErrorRateLimit{fmt.Sprintf("you can generate only %d rams per day, you can generate next in %d (unix)", len(config.Conf.Clicks.DailyRams), targetTime), 429, targetTime})
 		return errorGenerateRamLimitExceed
 	}
 	targetTime := user.DailyRamGenerationTime
@@ -161,7 +182,7 @@ func checkWsCanGenerateRam(ws *websocket.Conn, user entities.User) error {
 		targetTime += t * 60
 	}
 	if targetTime > int(time.Now().Unix()) {
-		ws.WriteJSON(wsErrorRateLimit{fmt.Sprintf("you can generate next ram in %d (unix)", targetTime), 429, targetTime})
+		WebsocketSendJSON(ctx, ws, wsErrorRateLimit{fmt.Sprintf("you can generate next ram in %d (unix)", targetTime), 429, targetTime})
 		return errorGenerateRamWaitTime
 	}
 	return nil
@@ -174,28 +195,28 @@ func (h *Handlers) websocketNeedClicks(ctx context.Context, ws *websocket.Conn, 
 	for {
 		select {
 		case <-ctx.Done():
-			ws.WriteJSON(wsError{"context canceled", 499})
+			WebsocketSendJSON(ctx, ws, wsError{"context canceled", 499})
 			return ctx.Err()
 		default:
 			messageType, wsMessage, err := ws.ReadMessage()
 			if err != nil {
-				ws.WriteJSON(wsError{"read message error", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"read message error", 400})
 				return err
 			}
 			if messageType != websocket.TextMessage {
-				ws.WriteJSON(wsError{"invalid message type", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"invalid message type", 400})
 				continue
 			}
 			messageClicks, err := strconv.Atoi(string(wsMessage))
 			if !ValidateClickData(messageClicks, lastClicks) {
-				ws.WriteJSON(wsError{"invalid clicks", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"invalid clicks", 400})
 				continue
 			}
 			lastClicks = time.Now()
 
 			clicked += messageClicks
 			if clicked >= amount {
-				ws.WriteJSON(map[string]any{"status": "success clicked"})
+				WebsocketSendJSON(ctx, ws, map[string]any{"status": "success clicked"})
 				return nil
 			}
 		}
@@ -218,8 +239,9 @@ func (h *Handlers) upgradedWebsocketClicker(ctx context.Context, ws *websocket.C
 	//TODO: defer Control Message
 
 	ctx, cancel := context.WithTimeout(ctx, time.Hour)
+	ctx = context.WithValue(ctx, "websocketSendMutex", &sync.Mutex{})
 	defer cancel()
-	userId, err := wsFirstMessageAuthorization(ws)
+	userId, err := wsFirstMessageAuthorization(ctx, ws)
 	if err != nil {
 		return
 	}
@@ -234,10 +256,10 @@ func (h *Handlers) upgradedWebsocketClicker(ctx context.Context, ws *websocket.C
 
 	if err = database.UpdateUserClickersBlockedIfCan(ctx, h.db, userId, int(time.Now().Unix())+7200, int(time.Now().Unix())); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			ws.WriteJSON(wsError{"cant tap or create 2 rams parallel", 409})
+			WebsocketSendJSON(ctx, ws, wsError{"cant tap or create 2 rams parallel", 409})
 			return
 		}
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return
 	}
 	defer database.UpdateUserClickersBlockedToZero(context.WithoutCancel(ctx), h.db, userId)
@@ -258,22 +280,22 @@ func (h *Handlers) upgradedWebsocketClicker(ctx context.Context, ws *websocket.C
 	for {
 		select {
 		case <-ctx.Done():
-			ws.WriteJSON(wsError{"context canceled", 499})
+			WebsocketSendJSON(ctx, ws, wsError{"context canceled", 499})
 			return
 		default:
 			messageType, wsMessage, err := ws.ReadMessage()
 			if err != nil {
-				ws.WriteJSON(wsError{"read message error", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"read message error", 400})
 				return
 			}
 			if messageType != websocket.TextMessage {
-				ws.WriteJSON(wsError{"invalid message type", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"invalid message type", 400})
 				continue
 			}
 
 			messageClicks, err := strconv.Atoi(string(wsMessage))
 			if !ValidateClickData(messageClicks, lastClicks) {
-				ws.WriteJSON(wsError{"invalid clicks", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"invalid clicks", 400})
 				continue
 			}
 			lastClicks = time.Now()
@@ -299,10 +321,11 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 	//TODO: defer Control Message
 
 	defer ws.Close()
+	ctx = context.WithValue(ctx, "websocketSendMutex", &sync.Mutex{})
 	ctx, cancel := context.WithTimeout(ctx, time.Hour)
 	defer cancel()
 
-	userId, err := wsFirstMessageAuthorization(ws)
+	userId, err := wsFirstMessageAuthorization(ctx, ws)
 	if err != nil {
 		return
 	}
@@ -310,17 +333,17 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 	if err != nil {
 		return
 	}
-	err = checkWsCanGenerateRam(ws, user)
+	err = checkWsCanGenerateRam(ctx, ws, user)
 	if err != nil {
 		return
 	}
 
 	if err = database.UpdateUserClickersBlockedIfCan(ctx, h.db, userId, int(time.Now().Unix())+7200, int(time.Now().Unix())); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			ws.WriteJSON(wsError{"cant tap or create 2 rams parallel", 409})
+			WebsocketSendJSON(ctx, ws, wsError{"cant tap or create 2 rams parallel", 409})
 			return
 		}
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return
 	}
 	defer database.UpdateUserClickersBlockedToZero(context.WithoutCancel(ctx), h.db, userId)
@@ -330,27 +353,28 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 	go PingOrCancelContext(ctx, ws, cancel)
 
 	if user.DailyRamGenerationTime == 0 {
-		ws.WriteJSON(map[string]any{"status": "need first ram prompt"})
+		WebsocketSendJSON(ctx, ws, map[string]any{"status": "need first ram prompt"})
 	} else {
-		ws.WriteJSON(map[string]any{"status": "need ram prompt"})
+		WebsocketSendJSON(ctx, ws, map[string]any{"status": "need ram prompt"})
 	}
 
 	messageType, wsMessage, err := ws.ReadMessage()
 	if err != nil {
-		ws.WriteJSON(wsError{"read message error", 400})
+		WebsocketSendJSON(ctx, ws, wsError{"read message error", 400})
 		return
 	}
 	if messageType != websocket.TextMessage {
-		ws.WriteJSON(wsError{"invalid message type", 400})
+		WebsocketSendJSON(ctx, ws, wsError{"invalid message type", 400})
 		return
 	}
 
 	userPrompt := string(wsMessage)
 	if len([]rune(userPrompt)) > config.Conf.Generation.MaxPromptLen {
-		ws.WriteJSON(wsError{fmt.Sprintf("user prompt too long (max %d symbols)", config.Conf.Generation.MaxPromptLen), 400})
+		WebsocketSendJSON(ctx, ws, wsError{fmt.Sprintf("user prompt too long (max %d symbols)", config.Conf.Generation.MaxPromptLen), 400})
 		return
 	}
 	aiGeneratedRam := make(chan entities.Ram)
+	defer close(aiGeneratedRam)
 	go func() {
 		var prompt string
 		if user.DailyRamGenerationTime == 0 {
@@ -358,7 +382,7 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 		} else {
 			rams, err := database.GetRamsByUserIdContext(ctx, h.db, user.Id)
 			if err != nil {
-				ws.WriteJSON(wsError{"unexpected db error", 500})
+				WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 				ws.Close()
 				return
 			}
@@ -370,16 +394,16 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 		}
 		if err != nil {
 			if errors.Is(err, ram_image_generator.CensorshipError) {
-				ws.WriteJSON(wsError{"user prompt or rams descriptions contains illegal content", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"user prompt or rams descriptions contains illegal content", 400})
 				ws.Close()
 				return
 			}
 			if errors.Is(err, ram_image_generator.TooLongPromptError) {
-				ws.WriteJSON(wsError{fmt.Sprintf("user prompt too long (max %d symbols)", config.Conf.Generation.MaxPromptLen), 400})
+				WebsocketSendJSON(ctx, ws, wsError{fmt.Sprintf("user prompt too long (max %d symbols)", config.Conf.Generation.MaxPromptLen), 400})
 				ws.Close()
 				return
 			}
-			ws.WriteJSON(wsError{"prompt generating error", 500})
+			WebsocketSendJSON(ctx, ws, wsError{"prompt generating error", 500})
 			ws.Close()
 			return
 		}
@@ -387,32 +411,32 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 		imageBase64, err := ram_image_generator.GenerateRamImage(ctx, h.gRPCClient, prompt)
 		if err != nil {
 			if errors.Is(err, ram_image_generator.ImageGenerationTimeout) {
-				ws.WriteJSON(wsError{"image generation timeout", 500})
+				WebsocketSendJSON(ctx, ws, wsError{"image generation timeout", 500})
 				ws.Close()
 				return
 			}
-			ws.WriteJSON(wsError{"prompt generating error", 500})
+			WebsocketSendJSON(ctx, ws, wsError{"prompt generating error", 500})
 			ws.Close()
 			return
 		}
 		imageUrl, err := ram_image_generator.UploadImage(imageBase64)
 		if err != nil {
-			ws.WriteJSON(wsError{"image uploading error", 500})
+			WebsocketSendJSON(ctx, ws, wsError{"image uploading error", 500})
 			ws.Close()
 			return
 		}
 		imageDescription, err := ram_image_generator.GenerateDescription(ctx, h.gRPCClient, imageUrl)
 		if err != nil {
 			if errors.Is(err, ram_image_generator.CensorshipError) {
-				ws.WriteJSON(wsError{"user prompt or rams descriptions contains illegal content", 400})
+				WebsocketSendJSON(ctx, ws, wsError{"user prompt or rams descriptions contains illegal content", 400})
 				ws.Close()
 				return
 			}
-			ws.WriteJSON(wsError{"image description generating error", 500})
+			WebsocketSendJSON(ctx, ws, wsError{"image description generating error", 500})
 			ws.Close()
 			return
 		}
-		ws.WriteJSON(map[string]string{"status": "image generated"})
+		WebsocketSendJSON(ctx, ws, map[string]string{"status": "image generated"})
 		aiGeneratedRam <- entities.Ram{UserId: user.Id, Description: imageDescription, ImageUrl: imageUrl}
 	}()
 
@@ -422,12 +446,12 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 		needClicks = config.Conf.Clicks.FirstRam
 	} else {
 		if ramsGeneratedLastDay >= len(config.Conf.Clicks.DailyRams) {
-			ws.WriteJSON(wsError{fmt.Sprintf("you can generate only %d rams per day", len(config.Conf.Clicks.DailyRams)), 429})
+			WebsocketSendJSON(ctx, ws, wsError{fmt.Sprintf("you can generate only %d rams per day, you can generate next in %d (unix)", len(config.Conf.Clicks.DailyRams), user.DailyRamGenerationTime+config.Conf.Generation.TimeBetweenDaily*60), 429})
 			return
 		}
 		needClicks = config.Conf.Clicks.DailyRams[ramsGeneratedLastDay]
 	}
-	ws.WriteJSON(map[string]any{"status": "need clicks", "clicks": needClicks})
+	WebsocketSendJSON(ctx, ws, map[string]any{"status": "need clicks", "clicks": needClicks})
 	err = h.websocketNeedClicks(ctx, ws, needClicks)
 	if err != nil {
 		return
@@ -436,24 +460,24 @@ func (h *Handlers) upgradedWebsocketGenerateRam(ctx context.Context, ws *websock
 	ram := <-aiGeneratedRam
 	tx, err := h.db.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return
 	}
 	ramId, err := database.CreateRamContext(ctx, tx, ram)
 	ram.Id = ramId
 	if err != nil {
 		tx.Rollback()
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return
 	}
 
 	err = database.UpdateUserContext(ctx, tx, user.Id, entities.User{DailyRamGenerationTime: int(time.Now().Unix()), RamsGeneratedLastDay: ramsGeneratedLastDay + 1})
 	if err != nil {
 		tx.Rollback()
-		ws.WriteJSON(wsError{"unexpected db error", 500})
+		WebsocketSendJSON(ctx, ws, wsError{"unexpected db error", 500})
 		return
 	}
 	tx.Commit()
-	ws.WriteJSON(ram)
+	WebsocketSendJSON(ctx, ws, ram)
 	return
 }
