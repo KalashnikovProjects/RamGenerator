ac52e41ba3c5ea29d912f8a57792abe49c3cd000
Maks <kalasmaks97@gmail.com>
2024-10-04 17:56:06 +0400

    Frontend: add footer.html on index page, header mover from base.html to header.html. Go-static-server: templates now renders once on program start, and stores in memory. Memory: O(requests now) -> O(pages count) CPU: O(requests) -> O(1)

--- Backend/Go-static-server/cmd/start_server.go
+++ Backend/Go-static-server/cmd/start_server.go
@@ -14,8 +14,17 @@ func main() {
 	logs.InitStdoutLogs(slog.LevelInfo)
 	config.InitConfigs()
 	ctx := context.Background()
-	serv := server.NewStaticServer(ctx, fmt.Sprintf(":%d", config.Conf.Port))
-	err := server.ServeServer(ctx, serv)
+
+	slog.Info("Rendering templates")
+	err := server.RenderTemplates()
+	if err != nil {
+		panic(err)
+	}
+	serv := server.NewStaticServer(fmt.Sprintf(":%d", config.Conf.Port))
+
+	slog.Info("Running static server", slog.String("addr", serv.Addr))
+
+	err = server.ServeServer(ctx, serv)
 	if err != nil {
 		slog.Error("Go static server shutdown with error", slog.String("error", err.Error()))
 		os.Exit(1)
--- Backend/Go-static-server/internal/server/server.go
+++ Backend/Go-static-server/internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/KalashnikovProjects/RamGenerator/Go-static-server/internal/config"
@@ -8,45 +9,94 @@ import (
 	"github.com/gorilla/mux"
 	"golang.org/x/sync/errgroup"
 	"html/template"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
 	"strconv"
 )
 
-type TemplateData struct {
-	Title string
+type ErrorTemplateData struct {
+	ErrorText string
 	config.BaseTemplateData
 }
 
-type ErrorTemplateData struct {
-	ErrorText string
-	TemplateData
+var Renders PagesRenders
+
+type PagesRenders struct {
+	Index    *[]byte
+	Login    *[]byte
+	User     *[]byte
+	Error404 *[]byte
 }
 
-func NewStaticServer(ctx context.Context, Addr string) *http.Server {
+func RenderTemplate(baseTemplates, additionalTemplates []string, templateData any) (*[]byte, error) {
+	ts, err := template.ParseFiles(append(baseTemplates, additionalTemplates...)...)
+	if err != nil {
+		return nil, err
+	}
+	buff := &bytes.Buffer{}
+	err = ts.Execute(buff, templateData)
+	if err != nil {
+		return nil, err
+	}
+	data, err := io.ReadAll(buff)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+func RenderTemplates() error {
+	baseTemplates := []string{fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "base.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "header.html")}
+	pagesRenders := PagesRenders{}
+
+	var err error
+	pagesRenders.Index, err = RenderTemplate(baseTemplates, []string{fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "index.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "footer.html")}, config.Conf.BaseTemplateData)
+	if err != nil {
+		return err
+	}
+	pagesRenders.Login, err = RenderTemplate(baseTemplates, []string{fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "login.html")}, config.Conf.BaseTemplateData)
+	if err != nil {
+		return err
+	}
+	pagesRenders.User, err = RenderTemplate(baseTemplates, []string{fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "user.html")}, config.Conf.BaseTemplateData)
+	if err != nil {
+		return err
+	}
+	pagesRenders.Error404, err = RenderTemplate(baseTemplates, []string{fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "error.html")}, ErrorTemplateData{"Такой страницы не существует", config.Conf.BaseTemplateData})
+	if err != nil {
+		return err
+	}
+	Renders = pagesRenders
+	return nil
+}
+
+func WriteRenderedPage(data *[]byte) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write(*data)
+	}
+}
+
+func NewStaticServer(Addr string) *http.Server {
 	router := mux.NewRouter()
 
-	router.NotFoundHandler = http.HandlerFunc(ErrorPage404)
+	router.NotFoundHandler = http.HandlerFunc(WriteRenderedPage(Renders.Error404))
 
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/users/{username}", User)
-
-	router.HandleFunc("/login", Login)
+	router.HandleFunc("/", WriteRenderedPage(Renders.Index))
+	router.HandleFunc("/users/{username}", WriteRenderedPage(Renders.User))
+	router.HandleFunc("/login", WriteRenderedPage(Renders.Login))
 
 	router.HandleFunc("/favicon.ico", faviconHandler)
 
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(config.Conf.Paths.CdnFiles))))
-
-	slog.Info("Creating static server", slog.String("addr", Addr))
 	return &http.Server{
 		Addr:    Addr,
 		Handler: tollbooth.LimitHandler(tollbooth.NewLimiter(50, nil), LoggingMiddleware(router)),
 	}
 }
-func ServeServer(ctx context.Context, server *http.Server) error {
-	slog.Info("Running static server", slog.String("addr", server.Addr))
 
+func ServeServer(ctx context.Context, server *http.Server) error {
 	g, gCtx := errgroup.WithContext(ctx)
 	g.Go(func() error {
 		err := server.ListenAndServe()
@@ -55,63 +105,6 @@ func ServeServer(ctx context.Context, server *http.Server) error {
 	})
 	<-gCtx.Done()
 	return server.Shutdown(ctx)
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles(fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "base.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "index.html"))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	err = ts.Execute(w, TemplateData{Title: fmt.Sprintf("Ram Generator"), BaseTemplateData: config.Conf.BaseTemplateData})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-}
-
-func User(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles(fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "base.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "user.html"))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	vars := mux.Vars(r)
-	err = ts.Execute(w, TemplateData{Title: fmt.Sprintf("%s - Ram Generator", vars["username"]), BaseTemplateData: config.Conf.BaseTemplateData})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles(fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "base.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "login.html"))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	err = ts.Execute(w, TemplateData{Title: "Ram Generator", BaseTemplateData: config.Conf.BaseTemplateData})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-}
-
-func ErrorPage404(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles(fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "base.html"), fmt.Sprintf("%s/%s", config.Conf.Paths.Templates, "error.html"))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	err = ts.Execute(w, ErrorTemplateData{ErrorText: "Такой страницы не существует", TemplateData: TemplateData{Title: fmt.Sprintf("404 - Ram Generator"), BaseTemplateData: config.Conf.BaseTemplateData}})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка чтения файла: %v", err), http.StatusInternalServerError)
-		return
-	}
 }
 
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
@@ -128,7 +121,6 @@ func faviconHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Устанавливаем заголовки HTTP
 	w.Header().Set("Content-Type", "image/x-icon")
 	w.Header().Set("Content-Length", strconv.FormatInt(fileInfo.Size(), 10))
 
